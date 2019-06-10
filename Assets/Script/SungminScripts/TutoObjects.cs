3fded06d9f6dc5d588e88e781c1222ced1ebe8d0
tankmon8 <tankmon8@naver.com>
2019-06-11 02:03:06 +0900

    - 튜토 씬 UI 제외하고 다 함

--- Assets/Script/SungminScripts/TutoObjects.cs
+++ Assets/Script/SungminScripts/TutoObjects.cs
@@ -8,26 +8,26 @@ public class TutoObjects : MonoBehaviour
     public GameObject[] arrow;
     private int arrowNum = 1;
 
-    public Image[] hintImage;
+    // public Image[] hintImage;
 
+    [SerializeField]
     private PlayerMovement playerInstance;
 
     public GameObject triggerObject;
 
     void Start()
     {
-        playerInstance = GetComponent<PlayerMovement>();
+        playerInstance = this.GetComponent<PlayerMovement>();
 
         for (int i = 1; i < arrow.Length; i++)
             arrow[i].SetActive(false);
     }
-
-    void OnCollisionEnter(Collision col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.name == "Arrow_" + arrowNum)
+        if (col.gameObject.name == "Arrow (" + arrowNum + ")")
         {
-            hintImage.SetValue(hintImage, arrowNum);
-            Destroy(col.gameObject); // 화살표 파괴할지 혹은 밑의 줄에
+            // hintImage.SetValue(hintImage, arrowNum);
+            //Destroy(col.gameObject); // 화살표 파괴할지 혹은 밑의 줄에
             arrow[arrowNum - 1].SetActive(false); // 화살표 오브젝트 풀링을 쓸지
             arrow[arrowNum].SetActive(true);
             arrowNum++;
@@ -35,11 +35,11 @@ public class TutoObjects : MonoBehaviour
 
         if (col.gameObject == triggerObject && playerInstance.isStickKeyDown)
         {
-            hintImage.SetValue(hintImage, 4);
+            // hintImage.SetValue(hintImage, 4);
             if (triggerObject.transform.rotation.y >= 150.0f) // 초기 값 저장하고 그거에 대해 150값 이동해야함ㅇ
                 arrowNum++;
         }
-        else
-            hintImage.SetValue(hintImage, arrowNum);
-    }
+        // else
+        // hintImage.SetValue(hintImage, arrowNum);
+    }    
 }
