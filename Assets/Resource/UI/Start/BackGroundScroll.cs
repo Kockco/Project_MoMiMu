29a8fcd3433d36e633164576ec64902e47c61bed
tankmon8 <tankmon8@naver.com>
2019-06-11 02:42:58 +0900

    - StartScene 작업

---
+++ Assets/Resource/UI/Start/BackGroundScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroll : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    public Material mat;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offSetY = mat.mainTextureOffset.y + scrollSpeed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(0, offSetY);
+
+        mat.mainTextureOffset = newOffset;
+    }
+}
