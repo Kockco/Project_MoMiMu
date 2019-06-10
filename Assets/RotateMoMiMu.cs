12c39b2a2d8af78a2904161d23045a340cdadfba
tankmon8 <tankmon8@naver.com>
2019-06-11 02:52:47 +0900

    - 클립 받기위한 백업

---
+++ Assets/RotateMoMiMu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMoMiMu : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0, 0, 10 * Time.deltaTime + 2));
+    }
+}
