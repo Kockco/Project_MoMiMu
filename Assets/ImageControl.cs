16a38480244a4a58e72ab0a86ecad442a1024638
tankmon8 <tankmon8@naver.com>
2019-06-11 05:13:04 +0900

    Merge branch 'master' of https://github.com/Kockco/Project_MoMiMu
    # Conflicts:
    #	Assets/Resource/UI/Start/MiddleGroundTexture.mat
    #	Assets/Scenes/StartScene.unity

---
+++ Assets/ImageControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] arrowTrigger;
+
+    [SerializeField]
+    private Image momiPopImage;
+    [SerializeField]
+    private Sprite[] momiPops;
+
+    [SerializeField]
+    private Image momuPopImage;
+    [SerializeField]
+    private Sprite[] momuPops;
+
+    [SerializeField]
+    private Image allPopImage;
+    [SerializeField]
+    private Sprite[] allPops;
+
+    [SerializeField]
+    private GameObject momiPopObject;
+    [SerializeField]
+    private GameObject momuPopObject;
+    [SerializeField]
+    private GameObject allPopObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        momiPops = GetComponent<Sprite[]>();
+        momuPops = GetComponent<Sprite[]>();
+        allPops = GetComponent<Sprite[]>();
+
+        //momiPopObject = GameObject.Find("MoMiPop");
+        //momuPopObject = GameObject.Find("MoMuPop");
+        //allPopObject = GameObject.Find("AllPop");
+
+        allPopObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (arrowTrigger[0].activeInHierarchy == true)
+        {
+            momiPopImage.sprite = momiPops[0];
+        }
+        else if (arrowTrigger[1].activeInHierarchy == true)
+        {
+            momiPopImage.sprite = momiPops[1];
+            AllPopImage();
+        }
+
+        if (arrowTrigger[2].activeInHierarchy == true)
+        {
+            momuPopImage.sprite = momuPops[0];
+        }
+        else if (arrowTrigger[3].activeInHierarchy == true)
+        {
+            momuPopImage.sprite = momuPops[1];
+            AllPopImage();
+        }
+    }
+
+    void AllPopImage()
+    {
+        if (!arrowTrigger[1].activeInHierarchy && !arrowTrigger[3].activeInHierarchy)
+        {
+            momiPopObject.SetActive(false);
+            momuPopObject.SetActive(false);
+            allPopObject.SetActive(true);
+            allPopImage.sprite = allPops[0];
+        }
+    }
+}
--- Assets/Script/SungminScripts/TutoObjects.cs
+++ Assets/Script/SungminScripts/TutoObjects.cs
@@ -8,8 +8,6 @@ public class TutoObjects : MonoBehaviour
     public GameObject[] arrow;
     private int arrowNum = 1;
 
-    // public Image[] hintImage;
-
     [SerializeField]
     private PlayerMovement playerInstance;
 
@@ -26,8 +24,6 @@ public class TutoObjects : MonoBehaviour
     {
         if (col.gameObject.name == "Arrow (" + arrowNum + ")")
         {
-            // hintImage.SetValue(hintImage, arrowNum);
-            //Destroy(col.gameObject); // 화살표 파괴할지 혹은 밑의 줄에
             arrow[arrowNum - 1].SetActive(false); // 화살표 오브젝트 풀링을 쓸지
             arrow[arrowNum].SetActive(true);
             arrowNum++;
@@ -35,11 +31,8 @@ public class TutoObjects : MonoBehaviour
 
         if (col.gameObject == triggerObject && playerInstance.isStickKeyDown)
         {
-            // hintImage.SetValue(hintImage, 4);
             if (triggerObject.transform.rotation.y >= 150.0f) // 초기 값 저장하고 그거에 대해 150값 이동해야함ㅇ
                 arrowNum++;
         }
-        // else
-        // hintImage.SetValue(hintImage, arrowNum);
     }    
 }
